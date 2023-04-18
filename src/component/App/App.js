b367bcd2938b5f78bf2c201e449fe453bea95d72
loanthiho <loan.hothi24@student.passerellesnumeriques.org>
2023-04-18 16:17:14 +0700

    done

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,5 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
+
+import './App.css';
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Slider from '../Slider/Slider';
@@ -10,10 +10,10 @@ class App extends Component {
    render(){
   return (
     <div>
-     <Header/>
-     <Slider/>
-     <Content/>
-     <Footer/>
+      <Header/>
+      <Slider/>
+      <Content/>
+      <Footer/>
    </div>
   );
 }
